9d9778436cf66926490b9f8f1a7e207f028ccf62
kur7258 <kur6011@gmail.com>
2022-08-25 00:17:08 +0900

    firtst_cm

---
+++ src/student_information.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * 학생들의 이름과 학번을 가진 클래스
+ */
+class Student
+{
+    String name;
+    String no;
+}
+
+public class student_information {
+    public static void main(String[] args) {
+        ArrayList<Student> arrayOfStudents = new ArrayList<>();
+    }
+}
